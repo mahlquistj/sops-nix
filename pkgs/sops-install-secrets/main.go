b9dba80d5ccc799ebc1151aed43129fe8800c566
Mads Ahlquist Jensen <madsaj10@outlook.com>
2026-03-10 22:33:03 +0100

    fix wrong return

--- pkgs/sops-install-secrets/main.go
+++ pkgs/sops-install-secrets/main.go
@@ -432,7 +432,7 @@ func prepareSecretsDir(secretMountpoint string, linkName string, keysGID int, us
 	}
 	if err := os.Chmod(dir, 0751); err != nil {
 		_ = os.RemoveAll(dir)
-		return fmt.Errorf("cannot chmod temporary symlink directory '%s': %w", dir, err)
+		return nil, fmt.Errorf("cannot chmod temporary symlink directory '%s': %w", dir, err)
 	}
 	if !userMode {
 		if err := os.Chown(dir, 0, int(keysGID)); err != nil {
